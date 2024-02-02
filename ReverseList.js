904f48918175d30e3d5e771848bfa146e56e3599
Dodo <dodo.rottmann@web.de>
2024-02-02 22:54:10 +0100

    Added Palindrom.js and ReverseList.js

---
+++ ReverseList.js
@@ -0,0 +1,21 @@
+/**
+ * 
+ *
+ * Simple Solution for Palindrom.js or Leedcode Palindrom Question
+ * 
+ * 
+ * @param {List[]} list
+ * @return {List[]}
+ */
+var reverseList = function() {
+
+    const reverseList = [];
+    for (let i = list.length - 1; i >= 0; i--) {
+        reverseList.push(list[i]);
+    }
+
+    return reverseList;
+}
+
+const list = [1, 2, 3, 4, 5];
+console.log(reverseList());
