a39848b3525f42934c789064a21cb05b04d7d5ce
Dominik <dominik_rottmann@web.de>
2024-04-07 11:00:49 +0200

    new file: Blind75/DynamicProgramming/JumpGame.js, new file: Blind75/DynamicProgramming/UniquePaths.js,new file: LongestIncreasingSubsequence.js

---
+++ LongestIncreasingSubsequence.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findLengthOfLCIS = function (nums) {
+    let max = 0
+    let count = 0
+    for (let i = 0; i < nums.length; i++) {
+
+    }
+};
+
+const nums = [1, 3, 5, 4, 7]
+console.log(findLengthOfLCIS(nums))
